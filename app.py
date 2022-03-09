5c223cda0ba5a17042471492215c465d474171af
Anzo52 <79906843+Anzo52@users.noreply.github.com>
2022-03-09 12:43:57 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# API for converting between Fahrenheit and Celsius
+
+from converter import converts
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return '''<h1>API for converting between Fahrenheit and Celsius</h1>
+<p>/api/v1.0/convert?f=<i>fahrenheit</i>&c=<i>celsius</i></p>
+<p>/api/v1.0/convert?c=<i>celsius</i>&f=<i>fahrenheit</i></p>'''
+
+
+@app.route('/api/v1.0/convert', methods=['GET'])
+def convert():
+    if 'f' in request.args:
+        f = float(request.args['f'])
+        c = converts.f2c(f)
+        return jsonify({'f': f, 'c': c})
+    elif 'c' in request.args:
+        c = float(request.args['c'])
+        f = converts.c2f(c)
+        return jsonify({'c': c, 'f': f})
+    else:
+        return jsonify({'error': 'Missing parameter'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# curl -i http://localhost:5000/api/v1.0/convert?f=32
+# curl -i http://localhost:5000/api/v1.0/convert?c=0
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='FarCelConverter',
+    version='0.1',
+    description='Farhenheit to Celsius Converter',
+    author='Andrew Zollner',
+    packages=find_packages(),
+)
